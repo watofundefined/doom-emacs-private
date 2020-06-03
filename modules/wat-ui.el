b3bc9e28dfae63ce813a8f92e11d438ee1a9ab0c
Matej Duracka <watofundefined@o.o0.0>
2020-06-03 14:54:09 +0300

    Add center buffer functionality ~<SPC> t c~

--- modules/wat-ui.el
+++ modules/wat-ui.el
@@ -12,7 +12,6 @@
 
 (setq display-line-numbers-type nil)
 
-(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
 
 (setq frame-title-format
       '(""
@@ -21,5 +20,41 @@
          (let ((project-name (projectile-project-name)))
            (unless (string= "-" project-name)
              (format " | %s" project-name))))))
+
+;; Centering logic taken from https://bzg.fr/en/emacs-strip-tease.html/
+(defun w-ui--center ()
+  (set-fringe-mode
+   (/ (- (frame-pixel-width) (* 90 (frame-char-width)))
+      2))
+  (custom-set-faces
+   '(fringe ((t (:background nil))))))
+
+(defun w-ui--uncenter ()
+  (set-fringe-mode 0)
+  (custom-set-faces
+   ;; FIXME Remove hard-coded theme-specific color
+   '(fringe ((t (:background "#292929"))))))
+
+(defun w-ui-toggle-center ()
+  (interactive)
+  "Toggle Centered View"
+  (if (< fringe-mode 10)
+      (w-ui--center)
+    (w-ui--uncenter)))
+
+(add-hook 'window-configuration-change-hook
+          (lambda ()
+            (if (delq nil
+                      (let ((fw (frame-width)))
+                        (mapcar (lambda(w) (< (window-width w) (/ fw 2)))
+                                (window-list))))
+                (w-ui--uncenter))))
+
+(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
+
+(map! :leader
+      (:prefix-map ("t" . "toggle")
+       :desc "Center view"  "c" #'w-ui-toggle-center))
+
 
 (provide 'wat-ui)
