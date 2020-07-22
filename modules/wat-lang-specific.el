f6b5a6971408a7ac8781d8e2800030a3b25eb9d7
Matej Duracka <watofundefined@o.o0.0>
2020-07-22 09:11:23 +0300

    toggle prettier mode only once

--- modules/wat-lang-specific.el
+++ modules/wat-lang-specific.el
@@ -2,8 +2,13 @@
 
 (require 'prettier-js)
 
-(add-hook! 'js2-mode-hook 'prettier-js-mode)
-(add-hook! 'web-mode-hook 'prettier-js-mode)
+(defun turn-on-prettier ()
+  (unless (bound-and-true-p prettier-js-mode)
+    (prettier-js-mode)))
+
+(add-hook! 'js2-mode-hook #'turn-on-prettier)
+(add-hook! 'web-mode-hook #'turn-on-prettier)
+(add-hook! '+web-react-mode-hook #'turn-on-prettier)
 (add-hook! before-save-hook #'refmt-before-save nil t)
 
 (provide 'wat-lang-specific)
