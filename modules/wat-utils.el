30df7bab9fa3b4e3d7cfb515bd4dcc7c94911c8d
Matej Duracka <watofundefined@o.o0.0>
2020-07-22 09:22:01 +0300

    add interactive fn prompt-commit-type

--- modules/wat-utils.el
+++ modules/wat-utils.el
@@ -25,5 +25,30 @@
 (defun w-utils-windows-p ()
   (string-equal system-type "windows-nt"))
 
+(defun w-utils-prompt-commit-type ()
+  "Prompt user to pick a commit type.
+Taken from Angular contributing guide:
+https://github.com/angular/angular/blob/master/CONTRIBUTING.md"
+  (interactive)
+  (let ((options '("1) 'build'    Changes that affect the build system or external dependencies
+              (example scopes: gulp, broccoli, npm)"
+                   "2) 'ci'       Changes to CI configuration files and scripts
+              (example scopes: Travis, Circle, BrowserStack, SauceLabs)"
+                   "3) 'docs'     Documentation only changes"
+                   "4) 'feat'     A new feature"
+                   "5) 'fix'      A bug fix"
+                   "6) 'perf'     A code change that improves performance"
+                   "7) 'refactor' A code change that neither fixes a bug nor adds a feature"
+                   "8) 'style'    Changes that do not affect the meaning of the code
+              (white-space, formatting, missing semi-colons, etc)"
+                   "9) 'test'     Adding missing tests or correcting existing tests")))
+    (let ((choice (ivy-read "Commit type: " options)))
+      (let ((result (concat (first (s-match "\\w+" (first (s-match "'.*'" choice))))
+                            ": ")))
+      (insert result)))))
+
+(map! :after evil-magit
+      :map magit-mode-map
+      :n "?" #'w-utils-prompt-commit-type)
 
 (provide 'wat-utils)
